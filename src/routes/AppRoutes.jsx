edceb058b29adebcc9950b68d3c4d00f1b8ca2be
asraful0106 <asrafulapf@gmail.com>
2025-03-22 13:31:17 +0600

    feat: Error page for not found route has been created!

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import MainLayout from '../MainLayout';
 import SearchPage from '../pages/searchPage/SearchPage';
+import ErrorPage from '../pages/errorPage/ErrorPage';
 
 const AppRoutes = () => {
     return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
                 <Route path='/' element={<MainLayout/>}>
                     <Route path='/' element={<SearchPage />} />
                 </Route>
+                <Route path='*' element={<ErrorPage/>}/>
             </Routes>
         </BrowserRouter>
     );
